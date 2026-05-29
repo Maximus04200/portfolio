317ec0c10389484c8740ce455f87c982460ab93c
dev <martinlestoppeur@gmail.com>
2026-05-29 09:39:15 +0200

    page mentions légales - accordéon

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import MentionsLegales from "./pages/MentionsLegales";
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
--- src/pages/MentionsLegales.js
+++ src/pages/MentionsLegales.js
@@ -1,5 +1,53 @@
 const MentionsLegales = () => {
-  return <h1>MentionsLegales</h1>;
+  return (
+    <section className="container my-5">
+      <h2 className="text-center">Mentions légales</h2>
+      <div className="accordion" id="mentionsAccordion">
+
+        
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button className="accordion-button" data-bs-toggle="collapse" data-bs-target="#editeur">
+              Éditeur du site
+            </button>
+          </h2>
+          <div id="editeur" className="accordion-collapse collapse show">
+            <div className="accordion-body">
+              Contenu éditeur...
+            </div>
+          </div>
+        </div>
+
+        
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button className="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#hebergeur">
+              Hébergeur
+            </button>
+          </h2>
+          <div id="hebergeur" className="accordion-collapse collapse">
+            <div className="accordion-body">
+              Contenu hébergeur...
+            </div>
+          </div>
+        </div>
+
+
+<div className="accordion-item">
+          <h2 className="accordion-header">
+            <button className="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#credits">
+              Crédits
+            </button>
+          </h2>
+          <div id="credits" className="accordion-collapse collapse">
+            <div className="accordion-body">
+              Contenu crédits...
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default MentionsLegales;
